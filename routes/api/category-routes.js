9a20ec34fc4dab81b128f44d47b17ac13c027c3e
Dru Ludwig <druludwig@yahoo.com>
2021-07-29 20:39:54 -0700

    Setting restore point

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,18 +3,39 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+router.get('/', async (req, res) => {
+  try {
+    const allCats = await Category.findAll({
+      include: [{model:Product}],
+    });
+    res.status(200).json(allCats);
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+router.get('/:id', async (req, res) => {
+  try {
+    const getONEcat = await Category.findByPk(req.params.id, {
+        include: [{model:Product}],
+      });
+    if (!getONEcat) {
+      res.status(404).json({ message: 'No category found!' });
+      return;
+    }
+    res.status(200).json(getONEcat);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+router.post('/', async (req, res) => {
+  try {
+    const newCategory = await Reader.create(req.body);
+    res.status(200).json(newCategory);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
